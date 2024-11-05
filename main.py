af17d3cd9377f5a16f5b08a98af0c4d80d185b87
Alexndr <mugz_play@rambler.ru>
2024-11-05 22:27:20 +0400

    ver 0.1

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-class task:
-    def __init__(self, name, description, due_date, priority, status):
-        self.name = name
-        self.description = description
-        self.due_date = due_date
-        self.priority = priority
-        self.status = status
+from task import Task
+print("Добро пожаловать в меню" + "\n"+ "1 - Добавить задачу" + "\n" + "2 - Удалить задачу" + "\n" + "3 - Показать задачи" + "\n" + "4 - Выход")
+
+operation = input("ВВедите операцию: ")
+
+
+match operation:
+    case "1":
+        Task.add_task()
+    case "2":
+        Task.delete_task()
+    case "3":
+        Task.show_tasks()
+    case "4":
+        exit()
--- task.py
+++ task.py
@@ -1,4 +1,4 @@
-class task:
+class Task:
     def __init__(self, id, name, description, due_date, priority, status):
         self.id = id
         self.name = name
@@ -10,17 +10,13 @@ class task:
     def __str__(self):
         return f"Name: {self.name}\nDescription: {self.description}\nDue Date: {self.due_date}\nPriority: {self.priority}\nStatus: {self.status}"
     
-    def add_task(self, id, name, description, due_date, priority, status):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.due_date = due_date
-        self.priority = priority
-        self.status = status
+    def add_task():
+        name = input("Enter task name: ")
+        description = input("Enter task description: ")
+        due_date = input("Enter task due date: ")
+        priority = input("Enter task priority: ")
+        status = input("Enter task status: ")
+        return Task(None, name, description, due_date, priority, status)
         
-    def delete_task(self):
-        self.name = None
-        self.description = None
-        self.due_date = None
-        self.priority = None
-        self.status = None
+    def delete_task():
+        pass
